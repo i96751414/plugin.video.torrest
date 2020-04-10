f4d5a1b4b6d00ba4e29db399bfda55ef20a17d2b
i96751414 <i96751414@gmail.com>
2020-04-10 19:18:08 +0100

    Update platform configs

--- lib/os_platform.py
+++ lib/os_platform.py
@@ -16,7 +16,7 @@ class Arch:
     x64 = "x64"
     x86 = "x86"
     arm = "arm"
-    # arm64 = "arm64"
+    arm64 = "arm64"
     armv7 = "armv7"
 
 
@@ -31,17 +31,23 @@ def get_platform():
     system = platform.system().lower()
     version = platform.release()
     arch = Arch.x64 if sys.maxsize > 2 ** 32 else Arch.x86
-    machine = platform.machine()
+    machine = platform.machine().lower()
 
     if "ANDROID_STORAGE" in os.environ:
         system = System.android
         if "arm" in machine or "aarch" in machine:
-            arch = Arch.arm
+            if "64" in machine and arch == Arch.x64:
+                arch = Arch.arm64
+            else:
+                arch = Arch.arm
     elif system == System.linux:
         if "armv7" in machine:
             arch = Arch.armv7
         elif "arm" in machine:
-            arch = Arch.arm
+            if "64" in machine and arch == Arch.x64:
+                arch = Arch.arm64
+            else:
+                arch = Arch.arm
     elif system == System.windows:
         if machine.endswith("64"):
             arch = Arch.x64
@@ -52,8 +58,8 @@ def get_platform():
 
 
 def dump_platform():
-    return "system: {}\nrelease: {}\nmachine: {}\narchitecture: {}\n".format(
-        platform.system(), platform.release(), platform.machine(), platform.architecture())
+    return "system: {}\nrelease: {}\nmachine: {}\narchitecture: {}\nmax_size: {}".format(
+        platform.system(), platform.release(), platform.machine(), platform.architecture(), sys.maxsize)
 
 
 try:
