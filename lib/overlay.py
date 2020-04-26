dc5a640fffac4f4d0d954a13697e118aacca8ff3
i96751414 <i96751414@gmail.com>
2020-04-26 21:19:54 +0100

    Add padding to label

--- lib/overlay.py
+++ lib/overlay.py
@@ -7,7 +7,7 @@ from lib.kodi import WINDOW_FULLSCREEN_VIDEO, ADDON_PATH, get_resolution
 
 
 class OverlayText(object):
-    def __init__(self, w=0.5, h=0.15, y_offset=0):
+    def __init__(self, w=0.5, h=0.15, y_offset=0, label_padding=15):
         window_width, window_height = get_resolution()
         self._window = Window(WINDOW_FULLSCREEN_VIDEO)
         self._shown = False
@@ -18,7 +18,7 @@ class OverlayText(object):
         x = (window_width - w) // 2
         y = int((3 * window_height / 4) - (h / 2)) + y_offset
 
-        self._label = ControlLabel(x, y, 0, 0, "", alignment=0x2 | 0x4)
+        self._label = ControlLabel(x + label_padding, y + label_padding, 0, 0, "", alignment=0x2 | 0x4)
         self._background = ControlImage(
             x, y, 0, 0, os.path.join(ADDON_PATH, "resources", "images", "black.png"),
             colorDiffuse="0xD0000000")
@@ -29,8 +29,10 @@ class OverlayText(object):
         # them to the window
         for c in self._controls:
             c.setVisible(self._shown)
-            c.setWidth(w)
-            c.setHeight(h)
+        self._background.setWidth(w)
+        self._background.setHeight(h)
+        self._label.setWidth(w - 2 * label_padding)
+        self._label.setHeight(h - 2 * label_padding)
 
     def _set_visible(self, visible):
         self._shown = visible
